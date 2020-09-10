4bb9d5419e5b9245c87f095216067d6f43f10973
kunnan0 <44965232+kunnan0@users.noreply.github.com>
2020-09-10 18:58:45 +0800

    DAY 17 AND 18 MISSON
    简单实现小任务

---
+++ DAY 17 AND 18/3_GAME.js
@@ -0,0 +1,9 @@
+for(var i=1;i<=100;i++) {
+    if(i%3==0) {
+        console.log("PA");
+    } else if(i%10==3) {
+        console.log("PA");
+    } else {
+        console.log(i);
+    }
+}
---
+++ DAY 17 AND 18/99_1.js
@@ -0,0 +1,7 @@
+var res=0;
+for(var i=1;i<10;i++) {
+    for(var j=1;j<10;j++) {
+        res=i*j;
+        console.log("i*j= "+res);
+    }
+}
